506171d5aa85719e16c04ae3026175e24d24482f
Colin Torney <colin.j.torney@gmail.com>
2015-09-21 15:06:22 +0100

    misc changes

--- socialDive/runModel.py
+++ socialDive/runModel.py
@@ -10,33 +10,33 @@ M = MCMC(dive_model)
 
 
 M.sample(iter=12000, burn=500, thin=10,verbose=0)
-mcplot(M)
-
-plt.hist([M.trace('intrinsic_rate')[:]],label='intrinsic')
-plt.hist([M.trace('social_rate')[:]],label='social')
-plt.legend(loc='upper left')
-plt.xlim(0,0.2)
-plt.show()
-
-
-d1=M.trace('blind_angle')[:]
-
-
-
-bc = d1*180/3.142
-plt.hist(bc)
-plt.xlim(0,180)
-plt.show()
-
-plt.hist([M.trace('lag')[:]])
-plt.legend(loc='upper left')
-plt.xlim(0,5)
-plt.show()
-
-plt.hist([M.trace('dist')[:]],100)
-plt.legend(loc='upper left')
-plt.xlim(0,200)
-plt.show()
+##mcplot(M)
+#
+#plt.hist([M.trace('intrinsic_rate')[:]],label='intrinsic')
+#plt.hist([M.trace('social_rate')[:]],label='social')
+#plt.legend(loc='upper left')
+#plt.xlim(0,0.2)
+#plt.show()
+#
+#
+#d1=M.trace('blind_angle')[:]
+#
+#
+#
+#bc = d1*180/3.142
+#plt.hist(bc)
+#plt.xlim(0,180)
+#plt.show()
+#
+#plt.hist([M.trace('lag')[:]])
+#plt.legend(loc='upper left')
+#plt.xlim(0,5)
+#plt.show()
+#
+#plt.hist([M.trace('dist')[:]],100)
+#plt.legend(loc='upper left')
+#plt.xlim(0,200)
+#plt.show()
 
 
 M2 = MAP(dive_model)
